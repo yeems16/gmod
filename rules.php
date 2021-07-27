5dd3c05f1a1eeb9c39bb32df69b9280a39927902
yeems16 <yeems16@protonmail.com>
2021-07-26 22:18:33 -0700

    doin ur mom im obama

---
+++ rules.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/main.min.css">
+    <link rel="stylesheet" href="css/animations.min.css">
+    <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+</head>
+<body>
+    <div class="container">
+        <div class="jumbotron" style="margin: 50px auto 10px; max-width: 840px;">
+            <img src="img/rules.png" alt="" class="pull-right img-rounded" style="width: 46%; height: 46%;">
+            <h1 id="title" class="bigEntrance" style="font-size: 50px;">NAME's Server</h1>
+            <p class="lead">
+                Welcome to Garry's Mod Sandbox!<br>
+                <small>
+                    <ul style="line-height: 1.6;">
+                        <li>Don't mess with other Players.<br>  [Ex. Killing or Deleting Props]</li>
+                        <li>Don't Spam.<br>  [Ex. Mic, Chat and Props]</li>
+                        <li>Don't try to overflow the Server.<br>  [Ex. Spawning too Many NPCs]</li>
+                        <li>Don't use any Cheats/Exploits.</li>
+                        <li>Listen to Admins</li>
+                    </ul>
+                    Workshop Collection:
+                    <br>
+					<a href="http://steamcommunity.com/sharedfiles/filedetails/?id=XXXXXXXXX"><code >http://steamcommunity.com/sharedfiles/filedetails/?id=XXXXXXXXX</code></a>
+                </small>
+            </p>
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-1.10.1.min.js"></script>
+    <script src="js/vendor/bootstrap.min.js"></script>
+</body>
+</html>
